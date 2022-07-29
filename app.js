11663bf375e3b6fae8801b1db3430fb94107a145
Anita <anitaisbeta@gmail.com>
2022-07-29 15:12:21 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+let myPage = ""
+const businessinfoBtn = document.getElementById("businessinfo-btn");
+const firstEl = document.getElementById('firstEl')
+const additionalinfoBtn = document.getElementById("additionalinfo-btn");
+const secondEl = document.getElementById('secondEl')
+const securityBtn = document.getElementById("security-btn");
+const thirdEl = document.getElementById('thirdEl')
+const previewBtn = document.getElementById("preview-btn");
+const fourthEl = document.getElementById('fourthEl')
+
+businessinfoBtn.addEventListener('click', () => { 
+    myPage.value = ""
+    renderPage()
+}) 
+
+function renderPage() {
+    let pageItems = ""
+}
+    
+
+
+
+
+    
